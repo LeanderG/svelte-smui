adeaefdc0ff1620659d81d05b322a9cdb777898e
Leander Gilles <leander.gilles@prodyna.com>
2020-12-03 16:44:25 +0100

    Inital Commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,16 +1,34 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: "/",
+
+    src: "/_dist_",
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: ["@snowpack/plugin-svelte"],
   install: [
     /* ... */
   ],
   installOptions: {
-    /* ... */
+    rollup: {
+      plugins: [
+        require("rollup-plugin-svelte")({
+          emitCss: true,
+        }),
+        require("rollup-plugin-postcss")({
+          extract: true,
+          minimize: true,
+          use: [
+            [
+              "sass",
+              {
+                includePaths: ["./src/theme", "./node_modules"],
+              },
+            ],
+          ],
+        }),
+      ],
+    },
   },
   devOptions: {
     /* ... */
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Button from "@smui/button";
+</script>
+
+<style>
+  /* css will go here */
+</style>
+
+<Button>Just a Button</Button>
